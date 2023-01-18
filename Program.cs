82f640c34139fc3040048336c15c97575efb6bc0
Oleg <olmag@inbox.ru>
2023-01-19 02:55:15 +0300

    task15

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿begin:
+
+Console.Write("Введите число, обозначающее день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a < 1 || a > 7)
+{
+    System.Console.WriteLine("такого дня недели не существует");
+    goto begin;
+}
+
+switch (a)
+{
+    case 6:
+        {
+            System.Console.WriteLine("да");
+            break;
+        }
+
+    case 7:
+        {
+            System.Console.WriteLine("да");
+            break;
+        }
+
+    default:
+        {
+            System.Console.WriteLine("нет");
+            break;
+        }
+}
